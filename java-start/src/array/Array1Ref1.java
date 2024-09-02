cffe76087547d4bbcb48f72e0e11fb15088d7452
sabb12 <sabb0923@gmail.com>
2024-09-02 20:31:11 +0900

    배열 리펙토링

--- java-start/src/array/Array1Ref1.java
+++ java-start/src/array/Array1Ref1.java
@@ -3,7 +3,6 @@ package array;
 public class Array1Ref1 {
     public static void main(String[] args) {
         int[] students; //1. 배열 변수 선언
-
         students = new int[5]; //2. 배열 생성
 
         // 변수 값 대입
